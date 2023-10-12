b83f518a7bdcb29b229d97284deeebbad384c174
Abhishek Kumar <142666329+abhi352003@users.noreply.github.com>
2023-10-12 20:00:59 +0000

    var

---
+++ basics/var.js
@@ -0,0 +1,10 @@
+const accountID = 1233;
+var employeeEmail ="abhishek@30350";
+let employeename ="abhishek kumar";
+
+
+/*we don't use var instead of we used let to declare the variable because of scope */ 
+ employeeEmail="hdhfgdjgfjk";
+ //accountID=3434;
+console.log(accountID);
+console.table([accountID,employeeEmail,employeename]);
